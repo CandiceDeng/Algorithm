d8dbc2603e82aac8836ba1d35f238ba72d5504df
Nan Deng <dengnan@umich.edu>
2019-12-15 01:05:32 -0500

    12/15

--- CopyListWithRandomPointer.java
+++ CopyListWithRandomPointer.java
@@ -38,23 +38,6 @@ class Node {
     }
 };
 */
-
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node next;
-    public Node random;
-
-    public Node() {}
-
-    public Node(int _val,Node _next,Node _random) {
-        val = _val;
-        next = _next;
-        random = _random;
-    }
-};
-*/
 class Solution {
     public Node copyRandomList(Node head) {
         if (head==null){
